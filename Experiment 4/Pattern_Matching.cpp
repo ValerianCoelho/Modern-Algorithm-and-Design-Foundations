cc6dca1c2ca9acb78e9f73598810c528af340d74
Valerian Coelho <valerian27012004@gmail.com>
2023-03-27 23:46:37 +0530

    Code Formatted

--- Experiment 4/Pattern_Matching.cpp
+++ Experiment 4/Pattern_Matching.cpp
@@ -3,30 +3,26 @@
 
 using namespace std;
 
-int match(string sentence, string pattern)
-{
+int match(string sentence, string pattern){
     int matched;
-    for (int i = 0; i < sentence.length() - pattern.length() + 1; i++)
-    {
+    for (int i = 0; i < sentence.length() - pattern.length() + 1; i++){
         matched = 1;
-        for (int j = 0; j < pattern.length(); j++)
-        {
-            if (sentence[i + j] != pattern[j])
-            {
+        for (int j = 0; j < pattern.length(); j++){
+            if (sentence[i + j] != pattern[j]){
                 matched = 0;
                 break;
+                cout << "Enter your name " << endl
+                     << "Hello";
             }
         }
-        if (matched)
-        {
+        if (matched){
             return i;
         }
     }
     return -1;
 }
 
-int main()
-{
+int main(){
     int pos;
     string sentence, pattern;
     cout << "Enter the string : ";
@@ -37,12 +33,10 @@ int main()
 
     pos = match(sentence, pattern);
 
-    if (pos > -1)
-    {
+    if (pos > -1){
         cout << "Pattern found at index : " << pos;
     }
-    else
-    {
+    else{
         cout << "Pattern not Present in the String";
     }
     return 0;
